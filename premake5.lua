05b46976d6b4c192354caa476e4f884d15667456
Tim Johansson <totimom@live.se>
2020-04-21 11:43:51 +0200

    Base setup for the project.

---
+++ premake5.lua
@@ -0,0 +1,13 @@
+workspace "DV2556Project"
+    configurations { "Debug", "Release" }
+    location "Build"
+        
+    project "DV2556Project"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "Bin/%{cfg.buildcfg}"
+    files { "Source/**.*" }
+    vpaths {
+        ["Headers/"] = { "**.h", "**.hpp" },
+        ["Sources/"] = {"**.c", "**.cpp"}
+    }
