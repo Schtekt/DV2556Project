23fe09b7c7e0361b498b8896e46a3c7d0186a54a
Tim Johansson <totimom@live.se>
2020-04-30 16:17:26 +0200

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,28 @@
-solution "DV2556Project"
-    configurations { "Debug", "Release" }
+workspace "DV2556Project"
     location "Build"
-        
+    configurations { "Debug", "Release"}
+    architecture "x86"
+
+    filter "system:windows"
+        systemversion "latest"
+
+    staticruntime "on"
+    filter "configurations:Debug"
+        defines {"_DEBUG"}
+        symbols "on"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        optimize "on"
+        runtime "Release"
+
     project "DV2556Project"
-    kind "ConsoleApp"
-    language "C++"
-    targetdir "Bin/%{cfg.buildcfg}"
-    files { "Source/**.*" }
-    vpaths {
-        ["Headers/"] = { "**.h", "**.hpp" },
-        ["Sources/"] = {"**.c", "**.cpp"}
-    }
+        kind "ConsoleApp"
+        language "C++"
+        targetdir "Bin/%{cfg.buildcfg}"
+        files { "Source/**.*" }
+
+        vpaths {
+            ["Headers/"] = { "**.h", "**.hpp" },
+            ["Sources/"] = {"**.c", "**.cpp"}
+        }
